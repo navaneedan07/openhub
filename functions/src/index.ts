838286854ec6f1d8731a9a025e7bc17f806e4ff6
navaneedan07 <navaneedannaveen07@gmail.com>
2025-12-26 22:06:38 +0530

    Remove unused Cloud Functions code - using Gemini REST API instead

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,7 +10,6 @@
 import {setGlobalOptions} from "firebase-functions";
 import {onRequest} from "firebase-functions/https";
 import * as logger from "firebase-functions/logger";
-import { generateAIContent } from "./gemini";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
@@ -26,8 +25,6 @@ import { generateAIContent } from "./gemini";
 // In the v1 API, each function can only serve one request per container, so
 // this will be the maximum concurrent request count.
 setGlobalOptions({ maxInstances: 10 });
-
-export { generateAIContent };
 
 // export const helloWorld = onRequest((request, response) => {
 //   logger.info("Hello logs!", {structuredData: true});
